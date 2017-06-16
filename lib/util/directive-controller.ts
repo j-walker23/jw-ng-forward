c05a852788a83ee6b9e889b5861ef16c4ad23f6b
John Walker <john@chaosdevs.com>
2017-06-16 09:33:50 -0600

    chore(app): upgrade to angular 1.6

--- lib/util/directive-controller.ts
+++ lib/util/directive-controller.ts
@@ -17,43 +17,51 @@ import { componentHooks } from '../decorators/component'
 // object in order to generate the controller
 export function directiveControllerFactory(caller: any, injects: string[], controller: any, ddo: any, $injector: any, locals: any): any {
   // Create an instance of the controller without calling its constructor
-  let instance = Object.create(controller.prototype);
+  let instance = Object.create(controller.prototype)
 
-  componentHooks._beforeCtrlInvoke.forEach(hook => hook(caller, injects, controller, ddo, $injector, locals));
+  componentHooks._beforeCtrlInvoke.forEach(hook => hook(caller, injects, controller, ddo, $injector, locals))
 
   // Finally, invoke the constructor using the injection array and the captured
   // locals
-  $injector.invoke([...injects, controller], instance, locals);
+  $injector.invoke([...injects, controller], instance, locals)
 
-  componentHooks._afterCtrlInvoke.forEach(hook => hook(caller, injects, controller, ddo, $injector, locals));
+  componentHooks._afterCtrlInvoke.forEach(hook => hook(caller, injects, controller, ddo, $injector, locals))
 
   // Use a1atscript's inputsBuilder to add the getters/setters then sugar
-  // over `=` and `@` bindings
+  // over `=` and `@` binding
   for (let key in ddo.inputMap) {
-    inputsBuilder(instance, key, ddo.inputMap[key]);
+    inputsBuilder(instance, key, ddo.inputMap[key])
   }
   // Remember, angular has already set those bindings on the `caller`
   // argument. Now we need to extend them onto our `instance`. It is important
   // to extend after defining the properties. That way we fire the setters.
-  Object.assign(instance, caller);
+  Object.assign(instance, caller)
 
   // Outputs work similarly, but they need the raw $element and the $scope for
   // destroying output observables.
-  let { $element, $scope } : { $element: JQuery, $scope: angular.IScope } = locals;
-  outputsBuilder(instance, $element, $scope, ddo.outputMap || {});
+  let { $element, $scope } : { $element: JQuery, $scope: angular.IScope } = locals
+  outputsBuilder(instance, $element, $scope, ddo.outputMap || {})
 
   if (typeof instance.ngOnInit === 'function') {
-    instance.ngOnInit();
+    ddo.$onInit = instance.ngOnInit.bind(instance)
+  }
+
+  if (typeof instance.ngOnChanges === 'function') {
+    ddo.$onChanges = instance.ngOnChanges.bind(instance)
+  }
+
+  if (typeof instance.ngDoCheck === 'function') {
+    ddo.$doCheck = instance.ngDoCheck.bind(instance)
   }
 
   if (typeof instance.ngOnDestroy === 'function') {
-    $scope.$on('$destroy', instance.ngOnDestroy.bind(instance));
+    $scope.$on('$destroy', instance.ngOnDestroy.bind(instance))
   }
 
   if (typeof instance.ngAfterViewInit === 'function') {
-    ddo.ngAfterViewInitBound = instance.ngAfterViewInit.bind(instance);
+    ddo.ngAfterViewInitBound = instance.ngAfterViewInit.bind(instance)
   }
 
   // Return the controller instance
-  return instance;
+  return instance
 }
