b4958e105fae4ab455acc7fc0f4adc67d4db5a7d
John Walker <john@chaosdevs.com>
2018-12-19 03:59:51 -0700

    wip(test): npm linking

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,20 @@
 import CleanPlugin from 'clean-webpack-plugin'
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
 import { Configuration } from 'webpack'
+import yargs from 'yargs'
 
+let { mode = 'development' } = yargs.argv
 
 const dist = `${__dirname}/dist`
 
 const config: Configuration = {
-  context: __dirname,
-  bail: true,
-  entry: 'lib/index.ts',
-  devtool: 'eval-source-map',
+  mode,
+  entry: 'lib/writers.ts',
+  // devtool: 'eval-source-map',
+  // devtool: 'cheap-source-map',
   output: {
     path: dist,
-    filename: 'index.ts',
+    filename: 'index.js',
   },
   resolve: {
     extensions: ['.ts', '.js'],
@@ -23,26 +25,23 @@ const config: Configuration = {
   plugins: [
     new CleanPlugin([dist]),
   ],
+  stats: {
+    maxModules: 1000,
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.ts$/,
         exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
             options: {
               presets: [
-                ['@babel/env'],
+                ['@babel/env', { modules: 'cjs' }],
               ],
             },
           },
-        ],
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
           {
             loader: 'ts-loader',
             options: {
